403094386b255927dda9452b30ea4ff82a68ec80
agadesh-softdev <agadesh.softdev@gmail.com>
2026-01-20 22:44:28 +0530

    JAVA

---
+++ First.java
@@ -0,0 +1,11 @@
+public class First {
+	public static void main(String args[]) {
+		int cost,discount,final_Price;
+		cost = 10;    // actual cost
+		discount=2;
+		final_Price=cost-discount;
+		System.out.println(final_Price);
+		String name = "agadesh";
+		System.out.println("My name is "+name);
+	}
+}
