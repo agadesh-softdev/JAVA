6570e13c97fe6772a9caace91beb4ff41c4e32be
agadesh-softdev <agadesh.softdev@gmail.com>
2026-01-25 12:26:31 +0530

    update switch2.java

---
+++ Switch2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Switch2 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        char letter;
+        System.out.println( "Enter a Letter : ");
+        letter=scan.next().charAt(0);
+
+        switch(letter){
+            case 'a':
+            case 'e':
+            case 'i':
+            case '0':
+            case 'u':
+                System.out.println("You Entered a vowel");
+                break;
+            default:
+                System.out.println("It's not a vowel");
+        }
+
+    }
+}
