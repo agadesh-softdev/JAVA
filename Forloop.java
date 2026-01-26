1776dd3854d2cbeedb42caf109a9632979acca80
agadesh-softdev <agadesh.softdev@gmail.com>
2026-01-26 16:29:10 +0530

    update for loop

---
+++ Forloop.java
@@ -0,0 +1,12 @@
+public class Forloop {
+    public static void main(String args[]){
+        int i;
+        int n=10;
+        for(i=1;i<n;i++){
+            if(i%2==1){
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
