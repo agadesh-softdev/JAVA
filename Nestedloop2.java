9ae16f96c30261aab7311902d45717bf82be2f28
agadesh-softdev <agadesh.softdev@gmail.com>
2026-02-05 14:28:25 +0530

    update

---
+++ Nestedloop2.java
@@ -0,0 +1,17 @@
+public class Nestedloop2 {
+    public static void main(String[] args) {
+        int rows = 10;
+        int cols = 10;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (i == 1 || i == rows || j == 1 || j == cols) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
